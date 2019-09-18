3b0f1e9f97aac71af24a6b0131a0e116ddae3278
Vafa Ronaghi <vafaronaghi@googlemail.com>
2019-09-18 13:57:57 +0200

    get packages

---
+++ get_pkgs.sh
@@ -0,0 +1,7 @@
+go get        github.com/jordan-wright/email
+go get        github.com/prometheus/alertmanager/template
+go get        github.com/sfreiberg/gotwilio
+go get        golang.org/x/oauth2
+go get        golang.org/x/oauth2/google
+go get        google.golang.org/api/gmail/v1
+
