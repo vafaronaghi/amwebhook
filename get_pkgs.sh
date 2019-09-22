02d1b4a143599695212b3ca7a60d8c775d48e8e8
Vafa Ronaghi <vafaronaghi@googlemail.com>
2019-09-22 22:20:05 +0200

    added amwebhook.go and script

--- get_pkgs.sh
+++ get_pkgs.sh
@@ -4,4 +4,4 @@ go get        github.com/sfreiberg/gotwilio
 go get        golang.org/x/oauth2
 go get        golang.org/x/oauth2/google
 go get        google.golang.org/api/gmail/v1
-
+go get        github.com/galdor/go-cmdline
